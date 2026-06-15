69f2e5e4d21faf273de493620fc7753f0e58f015
Markov Efim <ef.87@mail.ru>
2026-06-15 10:59:52 +0400

    Update vcore_kernel.cu

--- vcore_kernel.cu
+++ vcore_kernel.cu
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2026 Efim Sergeevich Markov (ef.87@mail.ru)
+ * Licensed under the GNU Affero General Public License v3.0 (AGPL-3.0).
+ * 
+ * SPECIAL RESTRICTION: No use of this code and files (artifacts) is permitted 
+ * for the training of machine learning models or artificial intelligence 
+ * without explicit written permission.
+ */
+
 #include <cuda_runtime.h>
 #include <math.h>
 
