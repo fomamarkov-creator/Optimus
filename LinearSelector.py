433950a83a2b0dcde045688061cfadd98d0149b9
Markov Efim <ef.87@mail.ru>
2026-06-15 11:41:11 +0400

    Update LinearSelector.py

--- LinearSelector.py
+++ LinearSelector.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2026 Efim Sergeevich Markov (ef.87@mail.ru)
+# Licensed under the GNU Affero General Public License v3.0 (AGPL-3.0).
+# 
+# SPECIAL RESTRICTION: No use of this code and files (artifacts) is permitted 
+# for the training of machine learning models or artificial intelligence 
+# without explicit written permission.
+# 
+# COMMERCIAL CLAUSE: Any enterprise deployment requires a paid commercial license.
+# Full license text is available in the LICENSE file in the root directory.
 import torch
 import cupy as cp
 
